6d189516bf1da3606e21878964b93e9ca7aa60ba
Sebastiaan Kloos <sebastiaan@codecycler.com>
2021-06-01 17:36:42 +0200

    Possibly fixed issue with urls

--- src/Notion/Properties/URL.php
+++ src/Notion/Properties/URL.php
@@ -4,4 +4,18 @@ use Notion\PropertyBase;
 
 class URL extends PropertyBase
 {
+    public function value()
+    {
+        return $this->config->url;
+    }
+
+    public function set($value): void
+    {
+        $this->config->url = $value;
+    }
+
+    public function getValue()
+    {
+        return $this->config->url;
+    }
 }
