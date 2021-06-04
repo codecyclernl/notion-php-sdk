466ab3b32c836535616bd2329086c6a19a979bbf
Sebastiaan Kloos <sebastiaan@codecycler.com>
2021-06-04 15:19:18 +0200

    Date prop created WIP

--- src/Notion/Properties/Date.php
+++ src/Notion/Properties/Date.php
@@ -4,4 +4,19 @@ use Notion\PropertyBase;
 
 class Date extends PropertyBase
 {
+    public function value()
+    {
+        return $this->config->date->start;
+    }
+
+    public function set($value): void
+    {
+        $this->config->date->start = $value;
+        unset($this->config->date->end);
+    }
+
+    public function getValue()
+    {
+        return $this->config->date->start;
+    }
 }
