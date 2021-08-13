f1c7496e721001d58749a960fd5afa3953108fdd
Sebastiaan Kloos <sebastiaan@codecycler.com>
2021-08-13 17:56:59 +0200

    Added code to handle rich_text properties

--- src/Notion/Properties/RichText.php
+++ src/Notion/Properties/RichText.php
@@ -7,9 +7,32 @@ class RichText extends PropertyBase
     public function value()
     {
         if (!empty($this->config->rich_text)) {
-            return $this->config->rich_text[0]->plain_text;
+            return $this->config->rich_text[0]->text->content;
         } else {
             return '';
         }
+    }
+
+    public function set($value): void
+    {
+        if (!is_array($this->config->rich_text)) {
+            $this->config->rich_text = [];
+
+            $this->config->rich_text[] = (object) [
+                'type' => 'text',
+                'text' => (object) [
+                    'content' => $value,
+                ],
+            ];
+
+            return;
+        }
+
+        $this->config->rich_text[0]->text->content = $value;
+    }
+
+    public function getValue()
+    {
+        return $this->config->rich_text;
     }
 }
