2540959335ec5a6bec18f382ae45ceb0faa97e23
Sebastiaan Kloos <sebastiaan@codecycler.com>
2021-11-05 16:40:48 +0100

    Added basic support for people type block

--- src/Notion/Properties/People.php
+++ src/Notion/Properties/People.php
@@ -4,4 +4,12 @@ use Notion\PropertyBase;
 
 class People extends PropertyBase
 {
+    public function value()
+    {
+        if (is_array($this->config->people) && isset($this->config->people[0])) {
+            return $this->config->people[0]->name;
+        }
+
+        return null;
+    }
 }
