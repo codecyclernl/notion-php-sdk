156650b462b66d5cfad35367484e184e6f4c34c8
Mikhail Lisnyak <xpundel@gmail.com>
2022-02-13 12:58:18 +0300

    Implement value get/set functionality for Number and MultiSelect properties

--- src/Notion/Properties/MultiSelect.php
+++ src/Notion/Properties/MultiSelect.php
@@ -4,4 +4,35 @@ use Notion\PropertyBase;
 
 class MultiSelect extends PropertyBase
 {
+
+    public function value()
+    {
+        return $this->config->multi_select->selected_values ?? null;
+    }
+
+
+    public function set($value): void
+    {
+        if (!isset($this->config->multi_select)) {
+            $this->config->multi_select = (object)[];
+        }
+
+        if(!is_array($value)) $value = [$value];
+        $this->config->multi_select->selected_values = $value;
+    }
+
+    public function get()
+    {
+        if (isset($this->getValue()?->selected_values)) {
+            return [
+                'multi_select' => array_map(fn($el) => ['name' => (string)$el], $this->getValue()?->selected_values)
+            ];
+        }
+        return null;
+    }
+
+    public function getValue()
+    {
+        return $this->config->multi_select;
+    }
 }
