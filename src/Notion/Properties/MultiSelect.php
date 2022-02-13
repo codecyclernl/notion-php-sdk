182ac6ac48ef8a18da5686901c63664638d4b425
Mikhail Lisnyak <xpundel@gmail.com>
2022-02-13 17:49:39 +0300

    fix multiselect

--- src/Notion/Properties/MultiSelect.php
+++ src/Notion/Properties/MultiSelect.php
@@ -1,15 +1,10 @@
 <?php namespace Notion\Properties;
 
 use Notion\PropertyBase;
+use Illuminate\Support\Arr;
 
 class MultiSelect extends PropertyBase
 {
-
-    public function value()
-    {
-        return $this->config->multi_select->selected_values ?? null;
-    }
-
 
     public function set($value): void
     {
@@ -17,22 +12,13 @@ class MultiSelect extends PropertyBase
             $this->config->multi_select = (object)[];
         }
 
-        if(!is_array($value)) $value = [$value];
-        $this->config->multi_select->selected_values = $value;
-    }
-
-    public function get()
-    {
-        if (isset($this->getValue()?->selected_values)) {
-            return [
-                'multi_select' => array_map(fn($el) => ['name' => (string)$el], $this->getValue()?->selected_values)
-            ];
-        }
-        return null;
+        if (!is_array($value)) $value = [$value];
+        $this->config->multi_select = array_map(fn($el) => (object)['name' => (string)$el], $value);
     }
 
     public function getValue()
     {
-        return $this->config->multi_select;
+        $withoutOptions = Arr::except((array)$this->config->multi_select, 'options');
+        return count($withoutOptions) ? (object)$withoutOptions : null;
     }
 }
